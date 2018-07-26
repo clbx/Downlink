9a737ff8d71d149c2700eff885880e56fecde730
Clay Buxton <clay@clbx.io>
2018-07-26 18:49:40 -0400

    Added Downlink Test

---
+++ DownlinkTest/DownlinkTest/DownlinkTest.cpp
@@ -0,0 +1,14 @@
+﻿// DownlinkTest.cpp : Defines the entry point for the application.
+//
+
+#include "DownlinkTest.h"
+
+using namespace std;
+
+int main()
+{
+	int test;
+	cout << "Hello CMake." << endl;
+	cin >> test;
+	return 0;
+}
