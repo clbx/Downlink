85d5dc37de0de6ebee7f714ad41bf199a8c79a4a
Clay Buxton <clay@clbx.io>
2019-05-06 22:15:33 -0400

    rebirth baby

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <SDL2/SDL.h>
+#undef main
+
+#include "imgui.h"
+#include "imgui_sdl.h"
+
+int main()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* window = SDL_CreateWindow("SDL2 ImGui Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1600, 900, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	ImGui::CreateContext();
+	ImGuiSDL::Initialize(renderer, 1600, 900);
+
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, 100, 100);
+	{
+		SDL_SetRenderTarget(renderer, texture);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+		SDL_RenderClear(renderer);
+		SDL_SetRenderTarget(renderer, nullptr);
+	}
+
+	bool run = true;
+	while (run)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+
+		int wheel = 0;
+
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT) run = false;
+			else if (e.type == SDL_WINDOWEVENT)
+			{
+				if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+				{
+					io.DisplaySize.x = static_cast<float>(e.window.data1);
+					io.DisplaySize.y = static_cast<float>(e.window.data2);
+				}
+			}
+			else if (e.type == SDL_MOUSEWHEEL)
+			{
+				wheel = e.wheel.y;
+			}
+		}
+
+		int mouseX, mouseY;
+		const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+		// Setup low-level inputs (e.g. on Win32, GetKeyboardState(), or write to those fields from your Windows message loop handlers, etc.)
+		
+		io.DeltaTime = 1.0f / 60.0f;
+		io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
+		io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+		io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+		io.MouseWheel = static_cast<float>(wheel);
+
+		ImGui::NewFrame();
+
+        ImGui::Begin("Player");
+            ImGui::Text("$Tree Fiddy");
+        ImGui::End();
+
+        ImGui::Begin("Computer");
+            ImGui::Text("12 JigglyHertz");
+            ImGui::Text("50 JigglyBits of RAM");
+            ImGui::Text("15 JigglyBits HDD");
+        ImGui::End();
+
+        ImGui::Begin("Network");
+            ImGui::Text("beepbeepbeep");
+            ImGui::Text("boopboopboop");
+        ImGui::End();
+
+
+		SDL_SetRenderDrawColor(renderer, 114, 144, 154, 255);
+		SDL_RenderClear(renderer);
+
+		ImGui::Render();
+		ImGuiSDL::Render(ImGui::GetDrawData());
+
+		SDL_RenderPresent(renderer);
+	}
+
+	ImGuiSDL::Deinitialize();
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	ImGui::DestroyContext();
+
+	return 0;
+}
